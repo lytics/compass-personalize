83dd9e45f5c37954ee95e593be76b3ae22282c72
Christopher Thorn <chris.thorn@lytics.com>
2025-02-11 15:00:20 -0800

    maint: iterate on edge function

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,16 +1,27 @@
 export default function handler (request) {
     const parsedUrl = new URL(request.url)
-    const route = parsedUrl.pathname
+    const { searchParams } = parsedUrl
 
-    if (route === '/appliances') {
-        const response = {
-            cookies: request.cookies,
-            test: 'Hello from the edge!',
-            time: new Date().toISOString() // Use ISO format for consistency
-        }
-        return new Response(JSON.stringify(response), {
-            headers: { 'Content-Type': 'application/json' }
-        })
+    let response
+
+    if (searchParams.has('seer')) {
+        const seerValue = searchParams.get('seer')
+
+        // Remove 'seer' from the query params to rewrite the URL
+        searchParams.delete('seer')
+        const newUrl
+      = parsedUrl.origin + parsedUrl.pathname + searchParams.toString()
+
+        // Forward modified request without the 'seer' param
+        response = fetch(new Request(newUrl, request))
+
+        // Set a cookie in the response headers
+        response = new Response(response.body, response)
+        response.headers.set(
+            'Set-Cookie',
+            `seerid=${seerValue}; Path=/; SameSite=Lax`
+        )
+        return response
     }
 
     return fetch(request)
