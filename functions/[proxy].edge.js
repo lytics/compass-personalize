bbf7b7a8a198cbdf2894778d1ba77dc6418c16e8
Christopher Thorn <chris.thorn@lytics.com>
2025-02-11 15:24:34 -0800

    maint: iterate on cloud function

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -5,19 +5,22 @@ export default function handler (request) {
     if (searchParams.has('seer')) {
         const seerValue = searchParams.get('seer')
 
+        const searchString = searchParams.toString()
         // Remove 'seer' from the query params to rewrite the URL
         searchParams.delete('seer')
         const newUrl
-      = parsedUrl.origin + parsedUrl.pathname + searchParams.toString()
+      = parsedUrl.origin
+      + parsedUrl.pathname
+      + (searchString ? '?' + searchString : '')
 
         // Forward modified request without the 'seer' param
         return fetch(new Request(newUrl, request)).then((res) => {
+            const headers = new Headers(res.headers)
+            headers.set('seerid', seerValue)
             return new Response(res.body, {
-                ...res,
-                headers: {
-                    ...res.headers,
-                    seerid: seerValue
-                }
+                status: res.status,
+                statusText: res.statusText,
+                headers
             })
         })
     }
