4371aad881f6d101b2bd445d65339a34dab3bbd0
Christopher Thorn <chris.thorn@lytics.com>
2025-02-11 14:24:52 -0800

    maint: eslint

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,15 +1,15 @@
-export default function handler(request) {
-    const parsedUrl = new URL(request.url);
-    const route = parsedUrl.pathname;
+export default function handler (request) {
+    const parsedUrl = new URL(request.url)
+    const route = parsedUrl.pathname
 
-    if (route === "/appliances") {
+    if (route === '/appliances') {
         const response = {
-            time: new Date().toISOString(), // Use ISO format for consistency
-        };
+            time: new Date().toISOString() // Use ISO format for consistency
+        }
         return new Response(JSON.stringify(response), {
-            headers: { "Content-Type": "application/json" },
-        });
+            headers: { 'Content-Type': 'application/json' }
+        })
     }
 
-    return fetch(request);
+    return fetch(request)
 }
