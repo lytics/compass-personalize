8af7bdc11980629a9438232346b8883ba9739966
Christopher Thorn <chris.thorn@lytics.com>
2025-02-11 15:15:55 -0800

    maint: iterate on cloud function

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -2,8 +2,6 @@ export default function handler (request) {
     const parsedUrl = new URL(request.url)
     const { searchParams } = parsedUrl
 
-    let response
-
     if (searchParams.has('seer')) {
         const seerValue = searchParams.get('seer')
 
@@ -13,15 +11,13 @@ export default function handler (request) {
       = parsedUrl.origin + parsedUrl.pathname + searchParams.toString()
 
         // Forward modified request without the 'seer' param
-        response = fetch(new Request(newUrl, request))
-
-        // Set a cookie in the response headers
-        response = new Response(response.body, response)
-        response.headers.set(
-            'Set-Cookie',
-            `seerid=${seerValue}; Path=/; SameSite=Lax`
-        )
-        return response
+        return fetch(new Request(newUrl, request)).then((res) => {
+            res.headers.set(
+                'Set-Cookie',
+                `seerid=${seerValue}; Path=/; SameSite=Lax`
+            )
+            return res
+        })
     }
 
     return fetch(request)
