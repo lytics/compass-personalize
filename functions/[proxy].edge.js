08641fe0cbe9f93fb1487a75cf641fe02693d904
Christopher Thorn <chris.thorn@lytics.com>
2025-02-12 11:37:45 -0800

    maint: use _id instead

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -2,24 +2,20 @@ export default function handler (request) {
     const parsedUrl = new URL(request.url)
     const { searchParams } = parsedUrl
 
-    if (searchParams.has('seer')) {
-        const seerValue = searchParams.get('seer')
+    if (searchParams.has('_id')) {
+        const idValue = searchParams.get('_id')
 
         // Remove 'seer' from the query params to rewrite the URL
-        searchParams.delete('seer')
+        searchParams.delete('_id')
         searchParams.set('ts', Date.now().toString()) // cache buster
         const searchString = searchParams.toString()
         const newUrl = `${parsedUrl.origin}${parsedUrl.pathname}${
             searchString ? '?' + searchString : ''
         }`
 
-        // Set a cookie with 'seerid'
+        // Set a cookie with 'cs-personalize-user-ui'
         const headers = new Headers()
-        headers.append(
-            'Set-Cookie',
-            `seerid=${seerValue}; Domain=.${parsedUrl.hostname};;`
-        + `cs-personalize-user-uid=${seerValue};`
-        )
+        headers.append('Set-Cookie', `cs-personalize-user-uid=${idValue};`)
         headers.append('Location', newUrl)
 
         return new Response(null, {
